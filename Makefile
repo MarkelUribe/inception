111442b87557323d8991945563fc25d7f059e6a9
MarkelUribe <uribemarkel@gmail.com>
2025-05-20 16:10:25 +0200

    makefile broken

--- Makefile
+++ Makefile
@@ -1,6 +1,28 @@
-all:
+DUMMIES = DB_PWD WP_ADMIN_N WP_ADMIN_P WP_ADMIN_E WP_U_PASS
+
+all: SSL $(DUMMIES)
+
+SSL:
 	mkdir -p secrets
 	openssl req -x509 -nodes -days 365 -subj  "/C=CA/ST=QC/O=C Inc/CN=e.com" -newkey rsa:2048 -keyout ./secrets/selfsigned.key -out ./secrets/selfsigned.crt
 
-clean:
+$(DUMMIES):
+	echo "$(@F)=" > $(addsuffix .txt, $(addprefix secrets/, $(@)))
+
+build:
+	docker-compose -f srcs/docker-compose.yml up --build
+
+down: 
+	docker-compose -f ./srcs/docker-compose.yml down 
+	docker-compose -f srcs/docker-compose.yml down --volumes
+
+clean: down
+	docker system prune -af
+	docker volume prune -f
 	rm -fr secrets
+	sudo rm -fr /home/$(USER)/data
+
+
+fclean: clean
+
+re: clean all
--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -4,8 +4,9 @@ echo "Starting Wordpress script..."
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
-./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
-./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+./wp-cli.phar config create --dbname="$DB_NAME" --dbuser="$DB_USER" --dbpass="$DB_PWD" --dbhost=mariadb --allow-root
+./wp-cli.phar core install --url="$DOMAIN_NAME" --title="$WP_TITLE" --admin_user="$WP_ADMIN_N" --admin_password="$WP_ADMIN_P" --admin_email="$WP_ADMIN_E" --allow-root
+./wp-cli.phar user create  "$WP_U_NAME" "$WP_U_EMAIL" --user_pass="$WP_U_PASS" --role="$WP_U_ROLE" --allow-root
 echo "Wordpress setup done!"
 
 php-fpm7.4 -F
